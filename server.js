b74915027ea6c3ccd96b68662c51cf7f08bba3b2
Jermaine Miller <jmillerjr93@gmail.com>
2023-03-05 15:43:51 -0500

    Started Work on User register route

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req,res) => res.send('API Running'));
+app.use(express.json({extended: false}))
+
+app.get('/', (req, res) => res.send('API Running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
 
